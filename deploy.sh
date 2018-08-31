4827a2886ae911728655f6fc2e77d6c137d485c4
Jesse Suen <jessesuen@gmail.com>
2018-08-31 16:54:37 -0700

    Fix typo in depoy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update && sudo apt-get install google-cloud-sdk -yw
+sudo apt-get update && sudo apt-get install google-cloud-sdk -y
 
 git clone https://github.com/YadiraF/PRNet
 cd PRNet
